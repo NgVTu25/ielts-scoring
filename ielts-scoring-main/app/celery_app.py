4fb51842922e7ce7ec6296086f973e8ab2913d58
NGUYEN VAN TU <143719265+NgVTu25@users.noreply.github.com>
2025-10-31 14:34:24 +0700

    fix: running in docker (redis connection)

--- ielts-scoring-main/app/celery_app.py
+++ ielts-scoring-main/app/celery_app.py
@@ -9,11 +9,13 @@ import os
 import re
 
 MIN_ENGLISH_RATIO = 0.5
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://redis:6379/0")
 
 celery_app = Celery(
     "worker",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0"
+    broker=CELERY_BROKER_URL,
+    backend=CELERY_RESULT_BACKEND
 )
 
 
