bd6e0a7c97e96604dfb329b41b127a15effe3c25
NGUYEN VAN TU <143719265+NgVTu25@users.noreply.github.com>
2025-11-05 21:55:40 +0700

    fix: CORSMiddleware localhost:5173

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(title="Free AI IELTS Speaking Scoring System")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
